387c8aeaa52a4633516254bb310e570e10fd794a
Mamanov Emil <emil.mamanov97@gmail.com>
2023-11-14 16:27:52 +0600

    made level 1 modal with "x" button

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,22 +1,25 @@
-import React, { ReactNode } from 'react';
-import Backdrop from '../Backdrop/Backrop';
+import React from 'react';
 
-interface Props {
+interface Props extends React.PropsWithChildren{
     show: boolean;
     title: string;
     onClose: () => void;
-    children: ReactNode;
 }
 
 const Modal: React.FC<Props> = ({ show, title, onClose, children }) => {
     return (
         <>
-            <Backdrop show={show} onClick={onClose} />
-            <div className="modal show" style={{ display: show ? 'block' : 'none' }}>
+            <div
+                className="modal-backdrop show"
+                style={{ display: show ? 'block' : 'none' }}
+                onClick={onClose}
+            />
+            <div className={`modal ${show ? 'show' : ''}`} style={{ display: show ? 'block' : 'none' }}>
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
                             <h1 className="modal-title fs-5">{title}</h1>
+                            <button type="button" className="btn-close" aria-label="Close" onClick={onClose}></button>
                         </div>
                         <div className="modal-body">{children}</div>
                     </div>
