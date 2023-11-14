e8d3ef1aa9eb9fa55b92ea378cc357fe14f26c6d
Mamanov Emil <emil.mamanov97@gmail.com>
2023-11-14 16:27:27 +0600

    made level 1 modal withut "x" button

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Modal from './components/Modal/Modal';
+import Backdrop from './components/Backdrop/Backrop';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [showModal, setShowModal] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const cancel = () => setShowModal(false);
+
+    return (
+        <div className="container mt-5">
+            <h1>React Modal Demo</h1>
+            <button className="btn btn-primary" onClick={() => setShowModal(true)}>
+                Open Modal
+            </button>
+
+            <Modal show={showModal} title="Some Modal Title" onClose={cancel}>
+                <p>This is modal content</p>
+                <button className="btn btn-danger" onClick={cancel}>
+                    Close
+                </button>
+            </Modal>
+
+            {showModal && <Backdrop show={showModal} onClick={cancel} />}
+        </div>
+    );
 }
 
-export default App
+export default App;
